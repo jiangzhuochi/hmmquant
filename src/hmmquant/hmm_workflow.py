6be812568f6dd88b106137fef0f6f2aa14a6b7e3
jiangzhuochi <1053067021@qq.com>
2021-09-02 16:54:20 +0800

    🐛 估计模型仅用选定的指标

--- src/hmmquant/hmm_workflow.py
+++ src/hmmquant/hmm_workflow.py
@@ -16,67 +16,68 @@ import hmmquant
 DATA_DIR = Path(".") / "data"
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-url = (
-    "https://stock2.finance.sina.com.cn/futures/api/jsonp.php"
-    "?symbol=T2112&type=15=/InnerFuturesNewService.getFewMinLine"
-)
-res = requests.get(url)
-df = (
-    pd.DataFrame(json.loads(re.findall(r"\[.*?\]", res.text)[0]))
-    .rename(
-        columns={
-            "d": "datetime",
-            "o": "open",
-            "h": "high",
-            "l": "low",
-            "c": "close",
-            "v": "volume",
-            "p": "position",
-        }
-    )
-    .set_index("datetime")
-    .rename(index=lambda idx: pd.to_datetime(idx))
-    .astype(
-        {
-            **{col_name: "float64" for col_name in ["open", "high", "low", "close"]},
-            **{col_name: "int64" for col_name in ["volume", "position"]},
-        }
-    )
-)
 
-# 如果现在的时间在 df 最后一行的时间之前
-# 就抛弃最后一行
-if (datetime.now() - df.index[-1]).total_seconds() < 0:
-    df = df.head(-1)
+def get_data() -> pd.DataFrame:
+    """从网上爬数据, 转换成 DataFrame"""
+
+    url = (
+        "https://stock2.finance.sina.com.cn/futures/api/jsonp.php"
+        "?symbol=T2112&type=15=/InnerFuturesNewService.getFewMinLine"
+    )
+    res = requests.get(url)
+    df = (
+        pd.DataFrame(json.loads(re.findall(r"\[.*?\]", res.text)[0]))
+        .rename(
+            columns={
+                "d": "datetime",
+                "o": "open",
+                "h": "high",
+                "l": "low",
+                "c": "close",
+                "v": "volume",
+                "p": "position",
+            }
+        )
+        .set_index("datetime")
+        .rename(index=lambda idx: pd.to_datetime(idx))
+        .astype(
+            {
+                **{
+                    col_name: "float64" for col_name in ["open", "high", "low", "close"]
+                },
+                **{col_name: "int64" for col_name in ["volume", "position"]},
+            }
+        )
+    )
+    # 如果现在的时间在 df 最后一行的时间之前
+    # 就抛弃最后一行
+    if (datetime.now() - df.index[-1]).total_seconds() < 0:
+        df = df.head(-1)
+
+    return df
+
 
 """本文件输入不断增长的 15min 数据表, 输出信号
 前置条件: 15min 数据, 至少包括 OHLC"""
 
-################################
-# step 0 参数
-state_num = 4
-train_min_len = 170
-every_group_len = 340
-indicator_list = ["RSI"]
 
 ################################
 # step 1 读取数据计算指标
 
 # quarter_data = pd.read_csv(DATA_DIR / "quarter.csv", index_col=0, parse_dates=True)
-quarter_data = df
-close_se = quarter_data["close"]
-high_se = quarter_data["high"]
-low_se = quarter_data["low"]
-# print(close_se)
-LOGRR: pd.Series = hmmquant.utils.get_logrr(close_se)
-RSI: pd.Series = talib.RSI(close_se, timeperiod=14)  # type:ignore
-df = pd.DataFrame(
-    {
-        "close_se": close_se,
-        "LOGRR": LOGRR,
-        "RSI": RSI,
-    }
-).dropna()
+def calc_indicator(quarter_data: pd.DataFrame):
+
+    close_se = quarter_data["close"]
+    # print(close_se)
+    LOGRR: pd.Series = hmmquant.utils.get_logrr(close_se)
+    RSI: pd.Series = talib.RSI(close_se, timeperiod=14)  # type:ignore
+    return pd.DataFrame(
+        {
+            "close_se": close_se,
+            "LOGRR": LOGRR,
+            "RSI": RSI,
+        }
+    ).dropna()
 
 
 ################################
@@ -130,10 +131,10 @@ class ModelWithTime:
     _time: str
 
 
-def create_model_with_time():
+def create_model_with_time(only_indicator_df: pd.DataFrame):
 
     # 取 train_min_len 个估计模型
-    needed_df = df.tail(train_min_len)
+    needed_df = only_indicator_df.tail(train_min_len)
     start, *_ = needed_df.index
 
     train = needed_df
@@ -153,22 +154,27 @@ def create_model_with_time():
     return model_with_time
 
 
-if (model_path := DATA_DIR / "model.pkl").exists():
-    with open(model_path, "rb") as f:
-        model_with_time = pickle.load(f)
+def get_model(only_indicator_df: pd.DataFrame):
 
-    # 如果模型的创建时间到现在的时间段数大于模型估计间隔 every_group_len
-    # 重新用 train_min_len 段时间估计模型
-    if len(df[model_with_time._time :]) > every_group_len:
-        model_with_time = create_model_with_time()
-        model = model_with_time._model
-    # 模型仍在有效期
+    if (model_path := DATA_DIR / "model.pkl").exists():
+        with open(model_path, "rb") as f:
+            model_with_time = pickle.load(f)
+
+        # 如果模型的创建时间到现在的时间段数大于模型估计间隔 every_group_len
+        # 重新用 train_min_len 段时间估计模型
+        if len(only_indicator_df.loc[model_with_time._time :]) > every_group_len:
+            model_with_time = create_model_with_time(only_indicator_df)
+            model = model_with_time._model
+        # 模型仍在有效期
+        else:
+            model: hmm.GaussianHMM = model_with_time._model
+    # 不存在模型则直接估计
     else:
-        model: hmm.GaussianHMM = model_with_time._model
-# 不存在模型则直接估计
-else:
-    model_with_time = create_model_with_time()
-    model = model_with_time._model
+        model_with_time = create_model_with_time(only_indicator_df)
+        model = model_with_time._model
+
+    return model, model_with_time._time
+
 
 # print(model_with_time)
 
@@ -176,23 +182,41 @@ else:
 ################################
 # step 3 信号
 
-# 观测指标的 df
-ob_df = df[indicator_list].loc[model_with_time._time :]
-start, *_, end = ob_df.index
-# print(ob_df)
-ob_np = hmmquant.utils.normalization(ob_df, plus=2).values  # type:ignore
-_, state = model.decode(ob_np, algorithm="viterbi")
-# 只关心当前最后一个隐藏状态，我们用它来预测下一个状态是属于涨组还是跌组
-last_state = state[-1]
 
-r = hmmquant.utils.get_all_state_rr(LOGRR[start:end], state)
-state_group = hmmquant.model.distinguish_state(r, 1, 1)
+def emit_signal(
+    model: hmm.GaussianHMM, model_estimation_time: str, all_data: pd.DataFrame
+):
+    # 观测序列
+    ob_df = all_data[indicator_list].loc[model_estimation_time:]
+    start, *_, end = ob_df.index
+    # print(ob_df)
+    ob_np = hmmquant.utils.normalization(ob_df, plus=2).values  # type:ignore
+    _, state = model.decode(ob_np, algorithm="viterbi")
+    # 只关心当前最后一个隐藏状态，我们用它来预测下一个状态是属于涨组还是跌组
+    last_state = state[-1]
 
-if last_state in state_group.rise_state:
-    sig = 1
-elif last_state in state_group.fall_state:
-    sig = -1
-else:
-    sig = 0
+    r = hmmquant.utils.get_all_state_rr(all_data["LOGRR"][start:end], state)
+    state_group = hmmquant.model.distinguish_state(r, 1, 1)
 
-print(df.index[-1], sig, sep=",")
+    if last_state in state_group.rise_state:
+        sig = 1
+    elif last_state in state_group.fall_state:
+        sig = -1
+    else:
+        sig = 0
+    return sig
+
+
+################################
+# step 0 参数
+state_num = 4
+train_min_len = 170
+every_group_len = 340
+indicator_list = ["RSI"]
+
+all_data = calc_indicator(get_data())
+only_indicator_df = all_data[indicator_list]
+model, model_time = get_model(only_indicator_df)
+# print(model, model_time)
+sig = emit_signal(model, model_time, all_data)
+print(all_data.index[-1], sig, sep=",")
