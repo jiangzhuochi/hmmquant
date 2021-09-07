baa1ab757553aa350730acc0ab7e990e270db057
jiangzhuochi <1053067021@qq.com>
2021-09-07 14:37:32 +0800

    🐛 fix

--- src/hmmquant/summary.py
+++ src/hmmquant/summary.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 df = pd.read_csv("./logs/rr.log", index_col=0, names=["strategy", "long"])
 df.index = pd.to_datetime(df.index.values)
-cumrr = df.cumsum().loc[datetime.now().strftime(r"%Y-%m-%d")]
+cumrr = df.loc[datetime.now().strftime(r"%Y-%m-%d")].cumsum()
 print(cumrr)
 cumrr.plot()
 plt.show()
